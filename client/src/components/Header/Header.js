435426bf26ab50caa679b43b939b4063d6cd2aaf
Brian Zhou <brianhzhou@gmail.com>
2023-11-02 23:21:09 -0700

    add styling to navbar

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,26 +3,26 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <header>
+      <header className="header w-full bg-gray-900 text-white flex justify-between items-center z-50 p-0 m-0">
         <nav>
           <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
+            <li  className="inline-block">
+              <NavLink className="transition-all duration-300 ease-in-out text-white block py-4 px-2 hover:bg-gray-700" to="/">Home</NavLink>
             </li>
-            <li>
-              <NavLink to="/notifications">Notifications</NavLink>
+            <li  className="inline-block">
+              <NavLink className="transition-all duration-300 ease-in-out text-white block py-4 px-2 hover:bg-gray-700 bg-red-700" to="/notifications">Notifications</NavLink>
             </li>
-            <li>
-              <NavLink to="/progress">Progress</NavLink>
+            <li  className="inline-block">
+              <NavLink className="transition-all duration-300 ease-in-out text-white block py-4 px-2 hover:bg-gray-700 bg-red-700"  to="/progress">Progress</NavLink>
             </li>
-            <li>
-              <NavLink to="/content">Content</NavLink>
+            <li  className="inline-block">
+              <NavLink className="transition-all duration-300 ease-in-out text-white block py-4 px-2 hover:bg-gray-700 bg-red-700" to="/content">Content</NavLink>
             </li>
-            <li>
-              <NavLink to="/plans">Plans</NavLink>
+            <li  className="inline-block">
+              <NavLink className="transition-all duration-300 ease-in-out text-white block py-4 px-2 hover:bg-gray-700 bg-red-700" to="/plans">Plans</NavLink>
             </li>
-            <li>
-              <NavLink to="/login">Login</NavLink>
+            <li  className="inline-block">
+              <NavLink className="transition-all duration-300 ease-in-out text-white block py-4 px-2 hover:bg-gray-700" to="/login">Login</NavLink>
             </li>
           </ul>
         </nav>
