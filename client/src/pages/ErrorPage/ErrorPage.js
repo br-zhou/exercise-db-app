c2369556af9cb56598300bc0bf125d5f56eef9c1
Brian Zhou <brianhzhou@gmail.com>
2023-11-02 23:26:17 -0700

    add styling to error page

--- client/src/pages/ErrorPage/ErrorPage.js
+++ client/src/pages/ErrorPage/ErrorPage.js
@@ -2,12 +2,17 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>An Error Occurred</h1>
-      <h4>404 Not Found</h4>
-      <p>
-        Click <Link to="/">here</Link> to return to the home page
-      </p>
+    <div className="text-center">
+      <h1 className="font-black text-7xl">An Error Occurred</h1>
+      <h4 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        404 Not Found
+      </h4>
+      <Link
+        class="inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring"
+        to="/"
+      >
+        Go Back Home
+      </Link>
     </div>
   );
 };
