c08551b9a89829b14bf33d9c2c0d699437e570aa
Brian Zhou <brianhzhou@gmail.com>
2023-11-24 17:56:14 -0800

    move login page into seperate component (#22)

---
+++ client/src/components/LoginCard/LoginCard.js
@@ -0,0 +1,36 @@
+import React, { createRef } from "react";
+import { Link } from "react-router-dom";
+
+const LoginCard = (props) => {
+  const usernameRef = createRef();
+  const passwordRef = createRef();
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    props.handleSubmit({
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+    });
+  };
+
+  return (
+    <div className="w-80 px-0 pt-8 mx-auto">
+      <div className="relative bg-white mx-auto text-center shadow-md p-8 text-lg">
+        <h1 className="uppercase">Login</h1>
+        <form onSubmit={onFormSubmit}>
+          <input type="text" placeholder="username" ref={usernameRef} className="focus:outline-none bg-gray-200 w-full border-0 m-2 p-2 box-border text-sm" />
+          <input type="password" placeholder="password" ref={passwordRef} className="focus:outline-none bg-gray-200 w-full border-0 m-2 p-2 box-border text-sm" />
+          <button className="font-roboto uppercase focus:outline-none bg-green-500 w-full border-0  m-2 p-2 text-white text-sm cursor-pointer">
+            LOGIN
+          </button>
+
+          Click <Link to={props.registerHref} className="text-green-500">Here</Link> To Register!
+
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginCard;
--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,66 +1,16 @@
 import { useState, setState } from "react";
-import {Link} from "react-router-dom";
+import LoginCard from "../../components/LoginCard/LoginCard";
 const LoginPage = () => {
-  const handleSubmit = (event) => {};
-  const [loginData, setLoginData] = useState({
-    name: "",
-    email: "",
-    goal: "",
-    curWeight: 0,
-  });
-
-  const handleRegister = (event) => {
-    window.location.href='/register';
+  const handleSubmit = (event) => {
+    console.log(event);
   };
 
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Login</h1>
-      <form className="space-y-6" action="#" method="POST">
-        <div>
-          <label htmlFor="email" id="email" name="email" className="block text-sm font-medium leading-6 text-gray-900">
-            Email:{" "}
-          </label>
-          <div className="mt-2">
-            <input
-              type="email"
-              class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-              placeholder="joesmith@domain.com"
-              autoComplete="email"
-            />
-          </div>
-        </div>
-        <div className="flex items-center justify-between">
-          <label htmlFor="password" id="password" name="password" className="block text-sm font-medium leading-6 text-gray-900">
-            Password:{" "}
-          </label>
-        </div>
-        <div className="mt-2">
-        <input
-          type="password"
-          class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-          placeholder="**********"
-        />
-        </div>
-        <button
-          type="button"
-          id="submit"
-          class="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 bg-blue-700 focus:ring-inset  sm:max-w-xs sm:text-sm sm:leading-6 hover:bg-blue-900"
-          onClick={handleSubmit()}
-        >
-          Log in
-        </button>
-        <p className="mt-10 text-center text-sm text-gray-500" >Don't have an account? Register Here.</p>
-        <Link
-          className="btn btn-primary"
-          name="register"
-          id="register"
-          class="block text-center w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset  sm:max-w-xs sm:text-sm sm:leading-6 hover:bg-gray-400"
-          to="/register"
-        >
-          Register
-        </Link>
-      </form>
+      <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        Login
+      </h1>
+      <LoginCard handleSubmit={handleSubmit} registerHref="/register" />
     </div>
   );
 };
