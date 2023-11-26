182a2257b37e40f8dc6f984372a07b6b43d4674d
mashiq01 <mashiq01@students.cs.ubc.ca>
2023-11-25 22:57:16 -0800

    notification page changed. Content page unchanged. Notification page to change after merging with main

--- client/src/pages/NotificationsPage/NotificationsPage.js
+++ client/src/pages/NotificationsPage/NotificationsPage.js
@@ -1,9 +1,46 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import NotificationCard from "../../components/NotificationCard/NotificationCard";
-import TimedCard from "../../components/TimedCard/TimedCard";
+import { serverFetch } from "../../utils/api";
 
 
 const NotificationsPage = () => {
+  
+    const [cards, setCards] = useState([
+      { rid: 1, title: 'Water reminder', content: 'Have Water', timeout: 5000, isVisible: false }, 
+      { rid: 2, title: 'Rest reminder', content: 'Take some rest', timeout: 5000, isVisible: false }, 
+      { rid: 3, title: 'Sleep reminder', content: 'Please sleep now', timeout: 5000, isVisible: false }, 
+    ]);
+  
+
+  const onLoad = async () => {
+    const dummyData = await serverFetch("GET", "notifications-table");
+     setData(dummyData);
+    console.log(dummyData)
+    // todo: get client data
+  };
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+    const cardTimeouts = cards.map((card) =>
+      setTimeout(() => {
+        // Update the cards state to make the specific card visible
+        setCards((prevCards) =>
+          prevCards.map((prevCard) =>
+            prevCard.rid === card.rid ? { ...prevCard, isVisible: true } : prevCard
+          )
+        );
+      }, card.timeout)
+    );
+
+    // Clear timeouts if the component unmounts
+    return () => cardTimeouts.forEach((timeoutId) => clearTimeout(timeoutId));
+  }, [cards]);
 
     return (
         <div>
@@ -11,7 +48,32 @@ const NotificationsPage = () => {
             NOTIFICATIONS PAGE
           </div>
 
-          <TimedCard />
+
+    <div>
+      {cards.map((card) => (
+        card.isVisible && (
+          <NotificationCard
+            key={card.rid} // Use rid as the key
+            title={card.title}
+            content={card.content}
+          />
+        )
+      ))}
+    </div>
+
+    <div>
+      {data.map((card) => (
+        card.isVisible && (
+          <NotificationCard
+            key={card.rid} // Use rid as the key
+            title={card.title}
+            content={card.content}
+          />
+        )
+      ))}
+    </div>
+  
+
         </div>
       );
     };
