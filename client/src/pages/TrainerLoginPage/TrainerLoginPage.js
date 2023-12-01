872fc83d5a2a70ddafe9f0a3f7c02ea97b4a4c15
Brian Zhou <brianhzhou@gmail.com>
2023-12-01 12:25:59 -0800

    Add Feedback on login error (#72)
    * only free users see ads
    
    * add error feedback on login

--- client/src/pages/TrainerLoginPage/TrainerLoginPage.js
+++ client/src/pages/TrainerLoginPage/TrainerLoginPage.js
@@ -9,17 +9,19 @@ const TrainerLoginPage = () => {
     if (token) {
       console.log("trainer-token", token);
       navigate("/trainer-dashboard");
-    } 
+    }
   }, []);
 
   const handleSubmit = async (event) => {
     console.log(event);
-    let result = await serverPost('POST', 'trainer-login',event);
+    let result = await serverPost("POST", "trainer-login", event);
     console.log(result);
 
     if (result) {
       localStorage.setItem("trainer-token", JSON.stringify(result));
       navigate("/trainer-dashboard");
+    } else {
+      event.setError("Invalid Crednetials");
     }
   };
 
@@ -28,7 +30,12 @@ const TrainerLoginPage = () => {
       <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
         Trainer Login
       </h1>
-      <LoginCard handleSubmit={handleSubmit} registerHref="/trainer-register" trainerHref="/login" altHrefWord="User" />
+      <LoginCard
+        handleSubmit={handleSubmit}
+        registerHref="/trainer-register"
+        trainerHref="/login"
+        altHrefWord="User"
+      />
     </div>
   );
 };
