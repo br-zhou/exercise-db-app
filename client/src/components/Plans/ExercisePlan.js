75a7c1107b7754b32dfc769ce5b871c517e7f9e0
Brian Zhou <brianhzhou@gmail.com>
2023-11-30 16:01:39 -0800

    Implement Ad fontend (#59)
    * add client details to view
    
    * trainer can see client exercises
    
    * update register page
    
    * add delete button to fe
    
    * add new plan to trainer-fe
    
    * add back button to trainer page
    
    * implement login functionality fe
    
    * add autologout without token
    
    * create trainer login and reg page
    
    * fix token parse issue
    
    * fix homepage error
    
    * add edit button to exercise plan card
    
    * connect dlet delete to backend
    
    * trainer can create  new exercise plans
    
    * implement some of edit into fe
    
    * add trainer register
    
    * feedback when email already exists
    
    * update navbar, fix some glitches
    
    * implement ads

--- client/src/components/Plans/ExercisePlan.js
+++ client/src/components/Plans/ExercisePlan.js
@@ -2,33 +2,35 @@ import { useEffect, useState } from "react";
 import { serverPost } from "../../utils/api";
 import ExerciseCard from "./ExerciseCard";
 const ExercisePlan = (token) => {
-  const onLoad = async() => {
+  const onLoad = async () => {
     const result = await serverPost("POST", "exerciseplan-table", token);
-        setData(result);
-    }
+    setData(result);
+  };
 
-    const [data, setData] = useState();
+  const [data, setData] = useState();
 
-    useEffect(() => {
-        onLoad();
-      }, []);
+  useEffect(() => {
+    onLoad();
+  }, []);
 
-    if (data === undefined) {
-      return <div>Still Loading ...</div>
-    }
+  if (data === undefined) {
+    return <div>Still Loading ...</div>;
+  }
 
-    return (
-        <div>
-        {data.map(row => {
-            return (
-            <div className="card-body max-w-xs mx-auto mt-2 p-4 bg-white rounded-lg shadow-lg border-0">
-              <h3 className="font-semibold  text-center mb-2">Exercise Plan {row[0][0]}</h3>
-              <ExerciseCard exercise={row} />
-              </div>
-            )
-          })}
+  return (
+    <div>
+      {data.map((row) => {
+        return (
+          <div className="card-body max-w-xs mx-auto mt-2 p-4 bg-white rounded-lg shadow-lg border-0">
+            <h3 className="font-semibold  text-center mb-2">
+              Exercise Plan {row && row[0] && row[0][0]}
+            </h3>
+            <ExerciseCard exercise={row} />
           </div>
-    )
-}
+        );
+      })}
+    </div>
+  );
+};
 
 export default ExercisePlan;
--- client/src/pages/RootLayout.js
+++ client/src/pages/RootLayout.js
@@ -1,14 +1,29 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header/Header";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import AdBanner from "../components/AdBanner/AdBanner";
+import { serverPost } from "../utils/api";
 
 const RootLayout = () => {
   const { pathname } = useLocation();
+  const [adData, setAddata] = useState(null);
+  const token = JSON.parse(localStorage.getItem("token") || "{}");
 
   // Scroll to top on page change
   useEffect(() => {
     window.scroll(0, 0);
   }, [pathname]);
+
+  const loadAd = async () => {
+    const data = await serverPost("POST", "get-ad", token);
+    if (data) {
+      setAddata({title: data[1], href: data[2]});
+    }
+  };
+
+  useEffect(() => {
+    loadAd();
+  }, []);
 
   return (
     <div>
@@ -16,6 +31,7 @@ const RootLayout = () => {
       <main>
         <Outlet />
       </main>
+      {adData && <AdBanner {...adData} />}
     </div>
   );
 };
