ee68de33fdd53b74abf33601d46b329f17cfbfcc
mobak13 <mobak13@students.cs.ubc.ca>
2023-12-01 13:52:36 -0800

    DEBUG: Fix Nutrition page fix

--- client/src/components/Plans/NutritionPlan.js
+++ client/src/components/Plans/NutritionPlan.js
@@ -22,7 +22,7 @@ if (data === undefined) {
 
 return (
     <div>
-        <NutritionCard key={data[0][0]} carbs={data[0][1]} fats={data[0][2]} protein={data[0][3]} calories={data[0][4]}/>
+        {data[0] ? <NutritionCard key={data[0][0]} carbs={data[0][1]} fats={data[0][2]} protein={data[0][3]} calories={data[0][4]}/> : <div className="text-sm text-center text-gray-700 mb-4">No plans available</div>}
     </div>
 )
 }
